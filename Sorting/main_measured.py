d95768111b98df2183a3c41adfd68d9480ed37f6
CarlosZT <65248574+CarlosZT@users.noreply.github.com>
2022-09-24 22:08:31 -0500

    Update main_measured.py

--- Sorting/main_measured.py
+++ Sorting/main_measured.py
@@ -3,24 +3,30 @@ import numpy as np
 import timeit
 import os
 
+#The CDLL module requires an absolute path to locate the library
 path = os.getcwd() + "\\Sorting\\insort.so"
 path = path.replace('\\', '/')
 
-RANDOM_CASE =  np.random.randint(0, 10000, 10000)
-WORST_CASE = np.arange(10000, 0, -1)
-BEST_CASE = np.arange(0, 10000, 1)
+#Experiment cases to evaluate C and Python insertion sort implementations
+input_size = 10000
+RANDOM_CASE =  np.random.randint(0, input_size, input_size)
+WORST_CASE = np.arange(input_size, 0, -1)
+BEST_CASE = np.arange(0, input_size, 1)
 
+#Current case selected
 A = BEST_CASE
 
+#Dependences to the Ctypes
 libc = CDLL(path)
 libc.insort.argtypes = [POINTER(c_int), c_int]
 size = len(A)
 numbers_c = (c_int * size)(*A)
 
+#Function to call the C implementation
 def insort_c():
     libc.insort(numbers_c, c_int(size))
 
-
+#Function to call the Python implementation
 def insort_python():
     for j in range(1, len(A)):
         key = A[j]
@@ -31,13 +37,19 @@ def insort_python():
         A[i + 1] = key
 
 
-def main():
-    n_exec = 10
 
+def main():
+    print(f'Insertion sort experiment. Input size: {input_size}')
+    
+    #Number of times the timeit module will execute each function to calculate the average execution time
+    n_exec = 10
+    
+    #Experiment for C
     result_c = timeit.timeit(stmt='insort_c()', globals=globals(), number=n_exec)
     exec_time = result_c/n_exec
     print(f'Time C: {exec_time}')
-
+    
+    #Experiment for Python
     result_py = timeit.timeit(stmt='insort_python()', globals=globals(), number=n_exec)
     exec_time = result_py/n_exec
     print(f'Time Python: {exec_time}')
